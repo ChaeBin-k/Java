832396e105e4bf251663fb797b3f4698b6282796
WaLalRulRu <cb10428@naver.com>
2023-04-05 08:37:00 +0900

    java first commit

---
+++ Test/src/Test1.java
@@ -0,0 +1,10 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("깃허브 연동 됐니?");
+		System.out.println("씨발 다 안됌 이쩌라는겨야 진짜 ");
+	}
+
+}
